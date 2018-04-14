bb93d2e4340d603e33312db1acd49f02bc249ddf
Colin Brence <cbrence@ramapo.edu>
2018-04-14 18:24:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 var playlist = new Object({ artistname: 'nin', songTitle: 'wish' })
 
-//function updatePlaylist(playlist, artistName, songTitle) {
- // Object.assign({}, playlist, { artistName: [], songTitle:[] })
- // return playlist
-//}
 
 function updatePlaylist(playlist, artistName, songTitle) { playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName, songTitle) { delete.playlist[artistName] = songTitle
   return playlist
 }
